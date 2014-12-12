8c7b01cd8cad90db29c4bea0a12192ccc3313e35
Romário Beckman <romabeckman@gmail.com>
2014-12-12 16:50:26 -0300

    Ajuste para evitar cach e local

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header('Pragma: no-cache'); // HTTP 1.0.
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past.
+
+setlocale(LC_TIME, 'ptb', 'pt_BR', 'pt_BR.utf-8', 'pt_BR.iso-8859-1', 'portuguese');
+date_default_timezone_set('America/Sao_Paulo');
+
 /*
  * ---------------------------------------------------------------
  * APPLICATION ENVIRONMENT
